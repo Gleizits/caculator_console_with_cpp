6b0649f083cd8f8d3ea7db24d7ee8313b4f6e3b3
Gleizits <98473712+Gleizits@users.noreply.github.com>
2022-04-07 17:14:16 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "iostream"
+#include "conio.h"
+using namespace std;
+int main(){
+    float a, b, suma, minus, multiplication, division;
+    cout<<"Digit a number:"<<endl; cin>>a;
+    cout<<"Digit other number:"<<endl; cin>>b;
+    suma = a + b;
+    minus = a - b;
+    multiplication = a * b;
+    division = a / b;
+    cout<<"The plus is:";
+    cout<<suma<<endl;
+    cout<<"The minus is:";
+    cout<<minus<<endl;
+    cout<<"The multiplication is:";
+    cout<<multiplication<<endl;
+    cout<<"The division is:";
+    cout<<division<<endl;
+    getch();
+    return 0;
+}
+//This is all for the time being, at another time you will receive an update.
